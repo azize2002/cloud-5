186f4fba63c4d3c8b48e6131105b8544a7b6af38
jihenbo <jihenebo58@gmail.com>
2024-11-21 21:34:22 +0100

    1er ajout

---
+++ firebase.js
@@ -0,0 +1,9 @@
+import { initializeApp } from 'firebase/app'
+
+const app = initializeApp({
+  apiKey: "AIzaSyArg8DVQ0oMAxAcc2iohchBZjlLTSSGEJY", // TODO
+  authDomain: "mahmoud-mcsi.firebaseapp.com", // TODO
+  projectId: "mahmoud-mcsi", // TODO
+})
+
+export default app
